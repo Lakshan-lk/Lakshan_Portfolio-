1b728ff3f56d25280246fde457a854f614e72008
Lakshan Ekanayaka <lakshanekanayakam20@gmail.com>
2026-03-25 19:23:16 +0530

    update

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -7,11 +7,17 @@ export const SmoothScroll = () => {
   useEffect(() => {
     // Initialize Lenis strictly on the client side
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function 
-      syncTouch: true, // Smooth feel on touch devices
-      touchMultiplier: 2,
-    });
+      duration: 1.2,          // ලැප් එකේ scroll වෙන වේගය (1.2 කියන්නේ පට්ට smooth ගාණක්)
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,           // PC එකට Smooth scroll On කරනවා
+      mouseMultiplier: 1,     // Mouse wheel එකේ වේගය
+      
+      // 👇 මේ ටික තමයි ෆෝන් එකට අදාළ වෙන්නේ!
+      smoothTouch: false,     // ෆෝන් එකේ (touch) Smooth scroll Off කරනවා (Native scroll එක වැඩ කරන්න දෙනවා)
+      touchMultiplier: 1,     // බැරිවෙලා හරි smoothTouch: true කළොත්, මේක 1 කට වගේ අඩු කරන්න (කලින් 2 වගේ තිබ්බ නිසා වෙන්න ඇති වේගෙන් ගියේ)
+    } as any);
 
     // Request Animation Frame loop
     function raf(time: number) {
